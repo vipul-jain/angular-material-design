59a5984c968691cd4673dadf74c6c731c1c6bd8e
vipul-jain <vipul.jain0@gmail.com>
2015-02-16 17:47:58 +0530

    Added Password Validation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,31 +25,35 @@
     var HTTP_CONFLICT = 409;
 
     $scope.registerUser = function(){
-      CarglyPartner.createPartner($scope.user,
-        function(success) {
-          $scope.user = {
-            businessName:"",
-            businessZip:"",
-            businessTimezone:"",
-            businessUrl:"",
-            contactName:"",
-            contactEmail:"",
-            password:""
-          };
-          $rootScope.headerText = 'Signed in as ' + CarglyPartner.user.name;
-          $rootScope.isLoggedIn = true;
-          $state.go('VerifyUser');
-        },
-        function(failure) {
-          if (failure["status"] == HTTP_CONFLICT) {
-              $scope.error ='The email address provided is already associated with another partner account.';
-          }
-          else {
-              $scope.error='An unexpected error occurred on the server. Please reload the page and try again. If the problem continues, contact us at support@cargly.com.';
-          }
-          $scope.$apply();
+        if(!($scope.user.password == $scope.user.confirmPassword)){
+            $scope.error ='Password must be matched';
+        } else {
+          CarglyPartner.createPartner($scope.user,
+            function(success) {
+              $scope.user = {
+                businessName:"",
+                businessZip:"",
+                businessTimezone:"",
+                businessUrl:"",
+                contactName:"",
+                contactEmail:"",
+                password:""
+              };
+              $rootScope.headerText = 'Signed in as ' + CarglyPartner.user.name;
+              $rootScope.isLoggedIn = true;
+              $state.go('VerifyUser');
+            },
+            function(failure) {
+              if (failure["status"] == HTTP_CONFLICT) {
+                  $scope.error ='The email address provided is already associated with another partner account.';
+              }
+              else {
+                  $scope.error='An unexpected error occurred on the server. Please reload the page and try again. If the problem continues, contact us at support@cargly.com.';
+              }
+              $scope.$apply();
+            }
+          );
         }
-      );
     }
 
      $scope.resendConfimEmail = function(){
